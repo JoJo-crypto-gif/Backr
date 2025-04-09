28dd0e1bf82d9fa2a8fc89ad51ae908403d5c64c
Jojo <ayamcvrter@gmail.com>
2025-04-09 11:01:31 +0000

    Filterss

--- frontend/src/pages/CampaignsPage.tsx
+++ frontend/src/pages/CampaignsPage.tsx
@@ -1,11 +1,12 @@
 import Navbar from "../components/ui/navbar"; // if you're inside /pages/
-import InlineFilter from "../components/campaigns/filter" 
+import InlineFilter from "../components/ui/filter"; 
 
 
 const Campaigns = () => {
     return (
           <div>
              <Navbar />
+             <InlineFilter />
 
           </div>
     );
